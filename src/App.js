6e0743f68d0a0da7c4d5e47cb5647d9c841c0c84
pfelto <paul.r.felton@gmail.com>
2022-01-19 17:41:23 -0500

    Starting with markup of the list of large data. Will use pagination to display 1000 records in sections of 20

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="listContainer">
+        <div className="listItem">1</div>
+        <div className="listItem">2</div>
+        <div className="listItem">3</div>
+        <div className="listItem">4</div>
+        <div className="listItem">5</div>
+        <div className="listItem">6</div>
+        <div className="listItem">7</div>
+        <div className="listItem">8</div>
+        <div className="listItem">9</div>
+        <div className="listItem">10</div>
+        <div className="listItem">11</div>
+        <div className="listItem">12</div>
+        <div className="listItem">13</div>
+        <div className="listItem">14</div>
+        <div className="listItem">15</div>
+        <div className="listItem">16</div>
+        <div className="listItem">17</div>
+        <div className="listItem">18</div>
+        <div className="listItem">19</div>
+        <div className="listItem">20</div>
+        <div className="listItem">21</div>
+        <div className="listItem">22</div>
+        <div className="listItem">23</div>
+        <div className="listItem">24</div>
+        <div className="listItem">25</div>
+        <div className="listItem">26</div>
+        <div className="listItem">27</div>
+        <div className="listItem">28</div>
+        <div className="listItem">29</div>
+        <div className="listItem">30</div>
+      </div>
+      <div className="listNavigation">
+        <button>Back</button>
+        <button>Forward</button>
+      </div>
     </div>
   );
 }
